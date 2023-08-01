32edf59f13abefda1544d026f8f93113316202c7
Scott Hurst <scott@rscotthurst.com>
2023-07-31 21:15:32 -0700

    Lambda only

---
+++ lib/cdk_workshop-stack.ts
@@ -0,0 +1,15 @@
+import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+
+export class CdkWorkshopStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // defines an AWS Lambda resource
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.PYTHON_3_9,    // execution environment
+      code: lambda.Code.fromAsset('lambda'),  // code loaded from "lambda" directory
+      handler: 'hello.handler'                // file is "hello", function is "handler"
+    });
+  }
+}

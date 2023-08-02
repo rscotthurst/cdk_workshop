dc7efe04a57095d2a8b0f0b25607cf6c74abb7e5
Scott Hurst <scott@rscotthurst.com>
2023-08-01 22:02:11 -0700

    Fix tabs in cdk_workshop-stack.ts

--- lib/cdk_workshop-stack.ts
+++ lib/cdk_workshop-stack.ts
@@ -3,19 +3,19 @@ import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigw from 'aws-cdk-lib/aws-apigateway';
 
 export class CdkWorkshopStack extends cdk.Stack {
-  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // defines an AWS Lambda resource
-    const hello = new lambda.Function(this, 'HelloHandler', {
-      runtime: lambda.Runtime.PYTHON_3_9,    // execution environment
-      code: lambda.Code.fromAsset('lambda'),  // code loaded from "lambda" directory
-      handler: 'hello.handler'                // file is "hello", function is "handler"
-    });
+        // defines an AWS Lambda resource
+        const hello = new lambda.Function(this, 'HelloHandler', {
+          runtime: lambda.Runtime.PYTHON_3_9,    // execution environment
+          code: lambda.Code.fromAsset('lambda'),  // code loaded from "lambda" directory
+          handler: 'hello.handler'                // file is "hello", function is "handler"
+        });
 
-    // defines an API Gateway REST API resource backed by our "hello" function.
-    new apigw.LambdaRestApi(this, 'Endpoint', {
-      handler: hello
-    });
-  }
+        // defines an API Gateway REST API resource backed by our "hello" function.
+        new apigw.LambdaRestApi(this, 'Endpoint', {
+          handler: hello
+        });
+    }
 }

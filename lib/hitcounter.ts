c583b4c455714a211d3f4e56739c6bcf505dbcea
Scott Hurst <scott@rscotthurst.com>
2023-08-01 22:15:02 -0700

    Change runtime to Python. Still getting error.

--- lib/hitcounter.ts
+++ lib/hitcounter.ts
@@ -21,7 +21,7 @@ export class HitCounter extends Construct {
       });
 
       this.handler = new lambda.Function(this, 'HitCounterHandler', {
-          runtime: lambda.Runtime.NODEJS_14_X,
+         runtime: lambda.Runtime.PYTHON_3_9,
          handler: 'hitcounter.handler',
          code: lambda.Code.fromAsset('lambda'),
           environment: {
